e5a0b48c9ec35ebdf78b6f98f767508f83eaa80c
Dea-k <lee5250@fredonia.edu>
2018-05-21 23:05:01 -0400

    The user can get music from storage

--- MusicPlayerApp/app/src/main/java/com/example/musicplayerapp/MainActivity.java
+++ MusicPlayerApp/app/src/main/java/com/example/musicplayerapp/MainActivity.java
@@ -1,20 +1,82 @@
 package com.example.musicplayerapp;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.musicplayerapp.ui.main.MainFragment;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int EXTERNAL_STORAGE_PERMISSION_REQUEST = 1;
+    List<Song> songList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow();
+
+        executeRuntimePermission(); // Don't try to access files from here below
+
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction()
+//                    .replace(R.id.container, MainFragment.newInstance())
+//                    .commitNow();
+//        }
+    }
+
+    private void executeRuntimePermission() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) { // Needs to ask permission runtime after M
+            if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestStoragePermission();
+            }
+        }
+    }
+
+    private void requestStoragePermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.permission_title)
+                    .setMessage(R.string.permission_info)
+                    .setPositiveButton(R.string.allow, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_PERMISSION_REQUEST);
+                        }
+                    })
+                    .setNegativeButton(R.string.deny, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    }).create().show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, EXTERNAL_STORAGE_PERMISSION_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == EXTERNAL_STORAGE_PERMISSION_REQUEST) {
+            if(grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                // Permission denied
+                Toast.makeText(this, R.string.exit_app, Toast.LENGTH_SHORT).show();
+                finish();
+            } else {
+                songList = MediaHelper.getMusicFromStorage(this);
+                Toast.makeText(this, "Permission all went through!", Toast.LENGTH_LONG).show();
+            }
         }
     }
 }
---
+++ MusicPlayerApp/app/src/main/java/com/example/musicplayerapp/Song.java
@@ -0,0 +1,70 @@
+package com.example.musicplayerapp;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Song implements Parcelable {
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long id, String title, String artist) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    protected Song(Parcel in) {
+        id = in.readLong();
+        title = in.readString();
+        artist = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeLong(id);
+        parcel.writeString(title);
+        parcel.writeString(artist);
+    }
+
+    public static final Creator<Song> CREATOR = new Creator<Song>() {
+        @Override
+        public Song createFromParcel(Parcel in) {
+            return new Song(in);
+        }
+
+        @Override
+        public Song[] newArray(int size) {
+            return new Song[size];
+        }
+    };
+}
